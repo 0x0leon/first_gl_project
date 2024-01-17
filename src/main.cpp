1816d7dbaf05c4359d95dbb8d7af4fe24e6e1bf1
0x0leon <leonselz@proton.me>
2024-01-17 23:27:22 +0100

    debug bug

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,16 @@
 #include <iostream>
 
 
-using namespace std;
+
 
 
 int main(){
+    
+
+    std::cout << "Test" << std::endl;
+
+    std::cout << "Hello World" << std::endl;
 
 
-    cout << "Hello World" << endl;
+    return 0;
 }
